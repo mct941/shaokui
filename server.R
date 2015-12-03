872ea7c362d5b86beaeab1bbff946227d317ae5a
mct941 <mct941@yahoo.com>
2015-12-03 15:29:14 -0600

    removed desktop based commands

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 #shinyServer
-setwd("C:/users/mtsai/desktop")
+#setwd("C:/users/mtsai/desktop")
 library(gtools)
 library(magrittr)
 library(shiny)
@@ -10,7 +10,71 @@ library(readxl)
 library(shinyBS)
 library(lazyeval)
 library(rdrop2)
-source(file.path("C:","users","mtsai","desktop","shaokui","common.r"))
+
+
+
+decode <- function(x, search, replace, default = NULL) {
+ 
+    # build a nested ifelse function by recursion
+    decode.fun <- function(search, replace, default = NULL)
+        if (length(search) == 0L) {
+            function(x) if (is.null(default)) x else rep(default, length(x))
+        } else {
+            function(x) ifelse(x == search[1L], replace[1L],
+                                                decode.fun(tail(search,  -1L),
+                                                           tail(replace, -1L),
+                                                           default)(x))
+        }
+ 
+    return(decode.fun(search, replace, default)(x))
+}
+
+read.nonmem <- function(file, n=-1) {
+    ## auxiliary function to split text lines at blanks
+    my.split <- function(line, numeric=FALSE) {
+        pieces <- unlist(strsplit(line, split=" +"))[-1]
+
+        if( numeric )
+            return(as.numeric(pieces))
+        else
+            return(pieces)
+    }
+
+    cat(sprintf("Reading NONMEM data from '%s'\n", file))
+
+    lines <- readLines(file, n) # read file as text
+    cat(sprintf("- %d lines\n", length(lines)))
+
+    idx <- substring(lines,1,1)!="T"
+    cat(sprintf("- %d tables\n", sum(!idx)))
+
+    lines <- lines[idx] # strip lines starting with T (TABLE NO ...)
+
+    ## do we have header lines??
+    if( length(grep("^ +[A-Za-z]", lines[1]))>0 ) { # yes!
+        data <- sapply(lines[lines!= lines[1]], my.split, numeric=TRUE)
+        header <-  my.split(lines[1])
+        cat(sprintf("- file has column names (%s)\n", paste(header,collapse=", ")))
+    } else {                                        # no
+        data <- sapply(lines, my.split, numeric=TRUE)
+        header <- sprintf("Column%02d", 1:nrow(data)) # make fake column names
+        cat("- file has NO header names - creating names Column01, Column02, ...\n")
+    }
+    cat(sprintf("- %d columns\n", nrow(data)))
+
+    ## transpose data and make a data.frame
+    df <- data.frame(data[1,])
+    for( i in 2:nrow(data))
+        df <- cbind(df, data[i,])
+
+    ## set column and row names
+    rownames(df) <- NULL
+    colnames(df) <- header
+
+    cat("ok.\n")
+
+    return(df)
+}
 
 stat.func <-function(x) c(min(x,na.rm=T),mean(x,na.rm=T),max(x,na.rm=T))
 tmax.calc <- function(TIME,DV) TIME[which.max(DV)]
